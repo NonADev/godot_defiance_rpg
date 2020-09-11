ef8f88ce2c4d649052033c47bb0a3a8c74fcd8dd
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-09-10 23:52:31 -0300

    movimentação e idle adicionados por animationTree

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -1,9 +1,21 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 140
-const ACCELERATION = 180
-const FRICCTION = 400
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+const MAX_SPEED = 80
+const ACCELERATION = 500
+const FRICCTION = 500
 var motion = Vector2.ZERO
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+
+func _ready():
+	animationTree.active = true
 
 
 func _physics_process(delta):
@@ -12,9 +24,15 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	print(animationTree.get("parameters/Idle/blend_position"))
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		
+		animationState.travel("Run")
 		motion = motion.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		motion = motion.move_toward(Vector2.ZERO, FRICCTION * delta)
 	
 	motion = move_and_slide(motion)
